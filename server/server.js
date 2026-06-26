3962e5fee3d6b03b795e168ec1db6f080a1223b3
mohammadataf <atafmohd32@gmail.com>
2026-06-27 04:10:00 +0530

    Feature 2: Connect auth routes and controllers

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 import express from "express";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
-});
+app.use("/api/auth", authRoutes);
 
 app.listen(5000, () => {
-  console.log("Server started on port 5000");
+    console.log("Server started on port 5000");
 });
